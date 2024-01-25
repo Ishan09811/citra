fcbb0944ccb4f9bca69f69669c8eee00cdc8c5b5
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-01-25 18:52:42 +0530

    Update ``navigation-safe-args`` to 2.7.6

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -19,6 +19,6 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
     }
 }
